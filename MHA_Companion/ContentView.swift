769f9d8cf901a730a3666f898b31a1291e8b39d3
Fallen <fallen.cosplay@protonmail.com>
2021-09-01 10:32:39 -0500

    Initial commit

---
+++ MHA_Companion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MHA_Companion
+//
+//  Created by Wiemer on 9/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MHA_Companion/MHA_CompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MHA_CompanionApp.swift
+//  MHA_Companion
+//
+//  Created by Wiemer on 9/1/21.
+//
+
+import SwiftUI
+
+@main
+struct MHA_CompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
